7cf159743691612e76cb840b3d33c56a71f22aeb
Anastasiia <110532797+AnastasiiaDudnik@users.noreply.github.com>
2023-03-05 18:05:20 +0200

    Initial commit

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  display: flex;
+  flex-direction: column;
+
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid lightgrey;
+  margin-bottom: 15px;
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+  border: 2px solid lightgreen;
+  background-color: lightpink;
+`;
